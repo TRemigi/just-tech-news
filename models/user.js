ab51cb54d9b3f9869c64a465411e609150c8ea85
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-07-22 21:25:16 -0600

    installed bcrypt and hashed user password on creation using hook

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const {
     DataTypes
 } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create User model
 class User extends Model {}
@@ -49,7 +50,12 @@ User.init({
 
 }, {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        }
+    },
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
